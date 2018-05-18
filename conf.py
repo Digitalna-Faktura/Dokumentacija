d53479998de4accc99f9a3931e2044c6db854a92
Digitalna-Faktura <39366657+Digitalna-Faktura@users.noreply.github.com>
2018-05-18 09:45:52 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -40,6 +40,7 @@ release = '2018'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinxcontrib.fulltoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -75,7 +76,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
